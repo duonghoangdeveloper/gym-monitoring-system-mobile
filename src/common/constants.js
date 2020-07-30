1b19edba4592b4365d70b06e0ecf93b4becafe10
trinnguyen888 <trinnguyen888@gmail.com>
2020-07-30 12:58:42 +0700

    nothing

---
+++ src/common/constants.js
@@ -0,0 +1,34 @@
+// export const URI =
+//   process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
+//     ? 'http://192.168.1.167:7777'
+//     : 'https://gym-monitoring-system.herokuapp.com';
+
+export const TOKEN_KEY = 'gym-monitoring-system-token';
+
+export const CONNECTION_STATUSES = {
+  CONNECTED: 'CONNECTED',
+  CONNECTING: 'CONNECTING',
+  DISCONNECTED: 'DISCONNECTED',
+  RECONNECTING: 'RECONNECTING',
+};
+
+export const AUTH_ROLES = [
+  'CUSTOMER',
+  'TRAINER',
+  'MANAGER',
+  'GYM_OWNER',
+  'SYSTEM_ADMIN',
+];
+
+export const ALL_ROLES = [
+  'GUEST',
+  'CUSTOMER',
+  'TRAINER',
+  'MANAGER',
+  'GYM_OWNER',
+  'SYSTEM_ADMIN',
+];
+
+export const USER_GENDERS = ['MALE', 'FEMALE', 'OTHER'];
+
+export const DATE_FORMAT = 'DD/MM/YYYY';
