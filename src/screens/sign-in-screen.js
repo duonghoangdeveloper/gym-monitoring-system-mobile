2313b6ea26b6fd51d7de33020351b507084c3dae
trinnguyen888 <trinnguyen888@gmail.com>
2020-08-10 02:53:04 +0700

    'notifications'

--- src/screens/sign-in-screen.js
+++ src/screens/sign-in-screen.js
@@ -9,20 +9,14 @@ import {
   Platform,
   StyleSheet,
   TouchableOpacity,
-  View,
 } from 'react-native';
-import { Button, Divider, Icon, Input, Text } from 'react-native-elements';
+import { Button, Icon, Input, Text } from 'react-native-elements';
 import { useDispatch, useSelector } from 'react-redux';
 
-<<<<<<< HEAD
 import { registerForPushNotificationsAsync } from '../../App';
-=======
-import { CommonButton } from '../components/common-button';
->>>>>>> master
 import { CommonDismissKeyboardWrapper } from '../components/common-dismiss-keyboard-wrapper';
-import { CommonInputForm } from '../components/common-input-form';
 import { TOKEN_KEY } from '../constants/app';
-import { DIMENSIONS, scaleH, scaleV } from '../constants/dimensions';
+import { scaleH, scaleV } from '../constants/dimensions';
 import { SIGN_IN } from '../redux/user/user.types';
 
 export const SignInScreen = ({ navigation }) => {
@@ -32,13 +26,7 @@ export const SignInScreen = ({ navigation }) => {
   const [password, setPassword] = useState('');
   const [visible, setVisible] = useState(false);
   const [loading, setLoading] = useState(false);
-<<<<<<< HEAD
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
 
-=======
->>>>>>> master
   const handleSignInPress = async () => {
     setLoading(true);
     const expoPushToken = await registerForPushNotificationsAsync();
@@ -101,76 +89,69 @@ export const SignInScreen = ({ navigation }) => {
 
   return (
     <CommonDismissKeyboardWrapper>
-      <View
-        style={{
-          alignItems: 'stretch',
-          backgroundColor: 'white',
-          flex: 1,
-          padding: DIMENSIONS.PADDING,
-
-          // justifyContent: 'center',
-        }}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        style={styles.container}
       >
-        <KeyboardAvoidingView
-          behavior={Platform.OS === 'ios' ? 'padding' : null}
-          style={styles.container}
-        >
-          <Text h3 style={styles.title}>
-            Sign In
-          </Text>
-
-          <CommonInputForm
-            label="Username"
-            onChangeText={text => setUsername(text)}
-            placeholder="Enter username"
-            style={{
-              alignItems: 'center',
-              backgroundColor: 'black',
-              flex: 1,
-
-              // justifyContent: 'center',
-            }}
-            value={username}
-          />
-
-          <CommonInputForm
-            label="Password"
-            // error={error}
-            onChangeText={setPassword}
-            placeholder="Enter password"
-            secureTextEntry={!visible}
-            value={password}
-          />
-
-          <TouchableOpacity
-            onPress={() => setVisible(!visible)}
-            style={styles.passwordVisibleToggleButton}
-          >
+        <Text h3 style={styles.title}>
+          Sign In
+        </Text>
+        <Input
+          leftIcon={
             <Icon
               color="#7f7f7f"
-              name={visible ? 'eye' : 'eye-slash'}
-              size={18}
-              type="font-awesome-5"
+              name="user"
+              size={24}
+              style={styles.inputIcon}
+              type="font-awesome"
             />
-          </TouchableOpacity>
-
-          {/* <Divider style={{ backgroundColor: 'white', height: 40 }} /> */}
-          <CommonButton
-            label="Sign in"
-            loading={loading}
-            onPress={handleSignInPress}
-            style={{ marginTop: scaleV(40) }}
-          />
-          {/* <Divider style={{ backgroundColor: 'white', height: 100 }} /> */}
-        </KeyboardAvoidingView>
-      </View>
+          }
+          onChangeText={setUsername}
+          placeholder="Enter username"
+          value={username}
+        />
+        <Input
+          leftIcon={
+            <Icon
+              color="#7f7f7f"
+              name="lock"
+              size={24}
+              style={styles.inputIcon}
+              type="font-awesome"
+            />
+          }
+          onChangeText={setPassword}
+          placeholder="Enter password"
+          rightIcon={
+            <TouchableOpacity
+              onPress={() => setVisible(!visible)}
+              style={styles.passwordVisibleToggleButton}
+            >
+              <Icon
+                color="#7f7f7f"
+                name={visible ? 'eye' : 'eye-slash'}
+                size={20}
+                type="font-awesome-5"
+              />
+            </TouchableOpacity>
+          }
+          secureTextEntry={!visible}
+          type="password"
+        />
+        <Button
+          containerStyle={styles.signInButton}
+          loading={loading}
+          onPress={handleSignInPress}
+          title="Sign in"
+        />
+      </KeyboardAvoidingView>
     </CommonDismissKeyboardWrapper>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'stretch',
+    alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
     padding: 12,
@@ -182,9 +163,7 @@ const styles = StyleSheet.create({
 
   passwordVisibleToggleButton: {
     alignItems: 'flex-end',
-    // backgroundColor: 'black',
-    bottom: scaleV(55),
-    height: scaleH(44),
+    flex: 1,
     justifyContent: 'center',
     left: scaleH(270),
     marginBottom: scaleV(-50),
@@ -205,6 +184,5 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: '600',
     marginBottom: 24,
-    marginLeft: scaleH(100),
   },
 });
