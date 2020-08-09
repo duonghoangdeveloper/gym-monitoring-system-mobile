3b6246ff9537c80983f7c2310b1c310f93012322
TungOV <65596935+TungOV@users.noreply.github.com>
2020-08-10 05:18:03 +0700

    only me

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import Constants from 'expo-constants';
+// import Constants from 'expo-constants';
 import * as Font from 'expo-font';
-import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
+// import * as Notifications from 'expo-notifications';
+// import * as Permissions from 'expo-permissions';
 import React, { useEffect, useRef, useState } from 'react';
 import { Platform } from 'react-native';
 
@@ -11,35 +11,35 @@ import { AppFontWrapper } from './src/components/app-font-wrapper';
 import { AppNavigation } from './src/components/app-navigation';
 import { AppReduxWrapper } from './src/components/app-redux-wrapper';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-    shouldShowAlert: true,
-  }),
-});
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => ({
+//     shouldPlaySound: true,
+//     shouldSetBadge: false,
+//     shouldShowAlert: true,
+//   }),
+// });
 export default function App() {
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
-  useEffect(() => {
-    registerForPushNotificationsAsync();
-    notificationListener.current = Notifications.addNotificationReceivedListener(
-      notification => {
-        setNotification(notification);
-      }
-    );
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(
-      response => {
-        console.log(response);
-      }
-    );
+  // const [notification, setNotification] = useState(false);
+  // const notificationListener = useRef();
+  // const responseListener = useRef();
+  // useEffect(() => {
+  //   registerForPushNotificationsAsync();
+  //   notificationListener.current = Notifications.addNotificationReceivedListener(
+  //     notification => {
+  //       setNotification(notification);
+  //     }
+  //   );
+  //   responseListener.current = Notifications.addNotificationResponseReceivedListener(
+  //     response => {
+  //       console.log(response);
+  //     }
+  //   );
 
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener);
-      Notifications.removeNotificationSubscription(responseListener);
-    };
-  }, []);
+  //   return () => {
+  //     Notifications.removeNotificationSubscription(notificationListener);
+  //     Notifications.removeNotificationSubscription(responseListener);
+  //   };
+  // }, []);
   return (
     <AppApolloWrapper>
       <AppReduxWrapper>
@@ -51,34 +51,34 @@ export default function App() {
   );
 }
 
-export const registerForPushNotificationsAsync = async function registerForPushNotificationsAsync() {
-  let expoPushToken;
-  if (Constants.isDevice) {
-    const { status: existingStatus } = await Permissions.getAsync(
-      Permissions.NOTIFICATIONS
-    );
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    expoPushToken = await Notifications.getExpoPushTokenAsync();
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
+// export const registerForPushNotificationsAsync = async function registerForPushNotificationsAsync() {
+//   let expoPushToken;
+//   if (Constants.isDevice) {
+//     const { status: existingStatus } = await Permissions.getAsync(
+//       Permissions.NOTIFICATIONS
+//     );
+//     let finalStatus = existingStatus;
+//     if (existingStatus !== 'granted') {
+//       const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+//       finalStatus = status;
+//     }
+//     if (finalStatus !== 'granted') {
+//       alert('Failed to get push token for push notification!');
+//       return;
+//     }
+//     expoPushToken = await Notifications.getExpoPushTokenAsync();
+//   } else {
+//     alert('Must use physical device for Push Notifications');
+//   }
 
-  if (Platform.OS === 'android') {
-    Notifications.createChannelAndroidAsync('default', {
-      name: 'default',
-      priority: 'max',
-      sound: true,
-      vibrate: [0, 250, 250, 250],
-    });
-  }
-  console.log(expoPushToken);
-  return expoPushToken.data;
-};
+//   if (Platform.OS === 'android') {
+//     Notifications.createChannelAndroidAsync('default', {
+//       name: 'default',
+//       priority: 'max',
+//       sound: true,
+//       vibrate: [0, 250, 250, 250],
+//     });
+//   }
+//   console.log(expoPushToken);
+//   return expoPushToken.data;
+// };
